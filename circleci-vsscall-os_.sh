d27a42e1976075b66fc6c04ec65e2b2a3b4af26f
VssecLab <91882824+vsseclab@users.noreply.github.com>
2024-02-01 09:06:05 +0700

    Update and rename circleci-diag.os.sh to circleci-vsscall-os_.sh

--- circleci-vsscall-os_.sh
+++ circleci-vsscall-os_.sh
@@ -17,13 +17,13 @@ function os_darwin() {
 #==========================================
 
 # This env should have been set by a prior script
-if [ -n $CIRCLECI_DIAG_OS ]
+if [ -n $CIRCLECI_VSSCALL_OS ]
 then
-  echo "The OS is $CIRCLECI_DIAG_OS"
-  if [ $CIRCLECI_DIAG_OS == "Linux" ]
+  echo "The OS is $CIRCLECI_VSSCALL_OS"
+  if [ $CIRCLECI_VSSCALL_OS == "Linux" ]
   then
     os_linux
-  elif [ $CIRCLECI_DIAG_OS == "Darwin" ]
+  elif [ $CIRCLECI_VSSCALL_OS == "Darwin" ]
   then
     os_darwin
   fi
